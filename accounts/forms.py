d5eb06fdc9a39b248dbfddd0d12f2beb1f5f90d7
reina515 <rgh22@mail.aub.edu>
2025-10-12 00:27:26 +0300

    forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import User, Patient, Doctor, Pharmacy
 
 USER_TYPE_CHOICES = [
     ('patient', 'Patient'),
     ('doctor', 'Doctor'),
     ('pharmacy', 'Pharmacy'),
-    ('admin', 'Admin'),
 ]
 
 class UserTypeForm(forms.Form):
@@ -18,14 +17,36 @@ class UserTypeForm(forms.Form):
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
+    name = forms.CharField(max_length=120, required=True, label='Full Name')
+    phone_number = forms.CharField(max_length=30, required=False, label='Phone Number')
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'password1', 'password2')
+        fields = ('email', 'username', 'name', 'phone_number', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        self.user_type = kwargs.pop('user_type', None)
+        super().__init__(*args, **kwargs)
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
+        user.name = self.cleaned_data['name']
+        user.phone_number = self.cleaned_data.get('phone_number', '')
+        
+        # Set role based on user_type from session
+        if self.user_type:
+            user.role = self.user_type
+        
         if commit:
             user.save()
+            
+            # Create the corresponding subtype based on role
+            if user.role == User.Role.PATIENT:
+                Patient.objects.create(patient_id=user)
+            elif user.role == User.Role.DOCTOR:
+                Doctor.objects.create(doctor_id=user)
+            elif user.role == User.Role.PHARMACY:
+                Pharmacy.objects.create(pharmacy_id=user)
+        
         return user
